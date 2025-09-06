5346aa375ab83abf4657ebf011754ccff1e8e6af
Chris Nielson <ctniel1@gmail.com>
2025-09-05 21:12:18 -0600

    Implement Search Bar

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -7,12 +7,24 @@ export const PokemonList = () => {
   const classes = useStyles();
   const { pokemons, loading } = useGetPokemons();
 
-  console.log(useGetPokemons());
+  const [filteredPokemons, setFilteredPokemons] = React.useState(pokemons);
+
+  React.useEffect(() => {
+    setFilteredPokemons(pokemons);
+  }, [pokemons]);
+
+  
 
   return (
     <div className={classes.root}>
+      <input className={classes.searchInput} placeholder="Search Pokemon" onChange={(e) => {
+        const search = e.target.value.toLowerCase();
+        setFilteredPokemons(
+          pokemons.filter((p) => p.name.toLowerCase().includes(search))
+        );
+      }} />
       {loading && <div>Loading...</div>}
-      {pokemons.map((pkmn) => (
+      {filteredPokemons.map((pkmn) => (
         <ListItem pokemon={pkmn} key={pkmn.id} />
       ))}
     </div>
@@ -26,6 +38,17 @@ const useStyles = createUseStyles(
       textAlign: 'center',
       padding: '32px',
       boxSizing: 'border-box',
+    },
+    searchInput: {
+      padding: '8px 16px',
+      fontSize: 16,
+      width: '100%',
+      maxWidth: 400,
+      marginBottom: 24,
+      borderRadius: 8,
+      border: '1px solid #ddd',
+      boxSizing: 'border-box',
+      color: '#333',
     },
   },
   { name: 'PokemonList' }
